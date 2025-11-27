c70cf72559bfc306d59ac8d2d1c761d7b841160e
Talhah Maknun <md.talhahmaknun24@gmail.com>
2025-11-28 01:36:45 +0600

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, (process as any).cwd(), '');
   return {
     plugins: [react()],
     define: {
@@ -11,8 +11,7 @@ export default defineConfig(({ mode }) => {
     },
     build: {
       outDir: 'dist',
-      sourcemap: false,
-      chunkSizeWarningLimit: 1600
+      sourcemap: false
     }
   }
 })
