3917b7eaf851fd33e65562c12a81af2a6547249f
Talhah Maknun <md.talhahmaknun24@gmail.com>
2025-11-28 16:40:55 +0600

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,24 @@
 
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, (process as any).cwd(), '');
-  return {
-    plugins: [react()],
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.API_KEY || "")
-    },
-    build: {
-      outDir: 'dist',
-      sourcemap: false,
-      chunkSizeWarningLimit: 2000,
-      commonjsOptions: {
-        transformMixedEsModules: true,
-      },
-      rollupOptions: {
-        onwarn(warning, warn) {
-          if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
-            return
-          }
-          warn(warning)
-        }
-      }
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY || "")
+  },
+  resolve: {
+    alias: {
+      '@': '/'
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    chunkSizeWarningLimit: 3000,
+    commonjsOptions: {
+      transformMixedEsModules: true,
     }
   }
 })
-    
